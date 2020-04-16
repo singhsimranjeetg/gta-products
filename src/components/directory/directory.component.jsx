e1e8ebc7ddc06a5256f27bd5e40661d5c02242f6
simran <simran097guraya@gmail.com>
2020-04-15 21:01:35 -0400

    finished section 4, changed menu items stlyes

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,97 @@
+import React from "react"
+import "./directory.styles.scss"
+import MenuItem from "../menu-item/menu-item.component"
+
+//this component need class components because we need state of the directory menu
+class Directory extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            sections: [
+                {
+                    title: 'hats',
+                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    id: 1,
+                    linkUrl: 'shop/hats'
+                  },
+                  {
+                    title: 'jackets',
+                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                    id: 2,
+                    linkUrl: 'shop/jackets'
+                  },
+                  {
+                    title: 't-shirts',
+                    imageUrl: 'https://i.ibb.co/g3DNrhd/t-shirt.jpg',
+                    id: 3,
+                    linkUrl: 'shop/t-shirts'
+                  },
+                  {
+                    title: 'jeans',
+                    imageUrl: 'https://i.ibb.co/BP23NH6/jeans.jpg',
+                    id: 4,
+                    linkUrl: 'shop/jeans'
+                  },
+                  {
+                    title: 'joggers',
+                    imageUrl: 'https://i.ibb.co/s3WwF9y/jogger.jpg',
+                    id: 5,
+                    linkUrl: 'shop/joggers'
+                  },
+                             
+                  {
+                    title: 'shoes',
+                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                    id: 6,
+                    linkUrl: 'shop/shoes'
+                  },
+
+                  {
+                    title: 'womens',
+                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                    size: 'large',
+                    id: 7,
+                    linkUrl: 'shop/womens'
+                  },
+
+                  {
+                    title: 'mens',
+                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    size: 'large',
+                    id: 8,
+                    linkUrl: 'shop/mens'
+                  },
+                  {
+                    title: 'kids',
+                    imageUrl: 'https://i.ibb.co/k0gFsX2/kids.jpg',
+                    size: 'large',
+                    id: 9,
+                    linkUrl: 'shop/kids'
+                  }        
+            ]
+        }
+      }   //we have to close the constructor after the state and 
+
+      
+        render() {
+            return (
+                <div className = "directory-menu">
+                    {
+                        this.state.sections.map(({title, imageUrl, id, size}) => (
+                         <MenuItem key = {id} title = {title} imageUrl = {imageUrl} size = {size} />
+                          ) )
+
+                        }
+                </div>
+                
+                  
+              )  }
+
+    
+    }
+
+
+
+
+export default Directory 
+ 
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+import Directory from "../../components/directory/directory.component"
+
+//homepage - whole window object 
+//directory menu 
+
+const HomePage = () => (
+  <div className = "homepage">
+    <Directory />
+  </div>
+  
+);
+
+export default HomePage;
