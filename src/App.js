b2e59bf4f747d236d0218bf6c444657176a534d0
simran <simran097guraya@gmail.com>
2020-04-16 20:19:24 -0400

    added header component  in the app component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import HomePage from "./pages/homepage/homepage.component"
 import './App.css';
 import {Switch, Route} from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component"
+import Header from  "./components/header/header.component"
 
 function App() { //Route comp takes three attributes, exact means only that path(which exactly maches the path name), path is the url and component is name of the component
   return (       //switch comp only lets run the base url nothing else, but when use exact it only renders the hatspage even with the switch
     <div className="App">
+      
+      <Header  />
       <Switch>
       <Route exact  path = "/" component = {HomePage}  />
      <Route   path = "/hats" component = {HatsPage}  /> 
@@ -14,7 +17,7 @@ function App() { //Route comp takes three attributes, exact means only that path
       </Switch>
       
     </div>
-  );
+  );  //we are placing our header comp out of the swich bz, no matter what switch and route display, header comp will be there for all urls
 }
 
 function HatsPage(){
