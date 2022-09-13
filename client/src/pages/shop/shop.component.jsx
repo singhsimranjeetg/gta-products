3f8125b0bcdda054fe768a01b4b51dbcda696db0
Simranjeet Singh <simranjeet.singh@thestable.com>
2022-09-13 17:01:04 -0400

    added tailwindcss

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,66 +1,49 @@
-import React, {useEffect, lazy, Suspense, Profiler} from "react"
+import React, { useEffect, lazy, Suspense, Profiler } from 'react';
 
-import "./shop.styles.scss"
+import './shop.styles.scss';
 
-import { Route } from "react-router-dom"
-import {connect} from "react-redux"
+import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-import {fetchCollectionsStart} from "../../redux/shop/shop.actions"
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
-import Spinner from "../../components/spinner/spinner.component"
-import ErrorBoundary from "../../components/error-boundary/error-boundary.component"
+import Spinner from '../../components/spinner/spinner.component';
+import ErrorBoundary from '../../components/error-boundary/error-boundary.component';
 
+const CollectionPageContainer = lazy(() => import('../../components/collectionPage/collectionPage.container'));
+const ShopPageWithSearch = lazy(() => import('../../components/search/search.component'));
 
+const ShopPage = ({ fetchCollectionsStart, match }) => {
+  useEffect(() => {
+    fetchCollectionsStart();
+  }, [fetchCollectionsStart]);
+  console.log('match', match);
+  // throw Error
+  return (
+    <div className="shop-page">
+      <ErrorBoundary>
+        <Suspense fallback={<Spinner />}>
+          <Profiler
+            id="ShopPage"
+            onRender={(id, phase, actualduration) => {
+              console.log({
+                id,
+                phase,
+                actualduration
+              });
+            }}
+          >
+            <Route exact path={`${match.path}`} component={ShopPageWithSearch} />
+            <Route path={`${match.path}/:categoryId`} component={CollectionPageContainer} />
+          </Profiler>
+        </Suspense>
+      </ErrorBoundary>
+    </div>
+  );
+};
 
-const CollectionPageContainer = lazy( () => import('../../components/collectionPage/collectionPage.container') )
-const ShopPageWithSearch = lazy( () => import('../../components/search/search.component') )
+const mapDispatchToProps = (dispatch) => ({
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
+});
 
-const ShopPage = ({fetchCollectionsStart, match}) =>  {
-
-   useEffect(() => {
-      fetchCollectionsStart()
-   },[fetchCollectionsStart])
-      console.log('match',match)
-     // throw Error
-      return(  
-           
-         <div className = "shop-page">
-         <ErrorBoundary>
-         <Suspense fallback = {<Spinner />} >
-            <Profiler id = "ShopPage" onRender = {(id, phase, actualduration)  => {
-               console.log({
-                  id,
-                  phase,
-                  actualduration
-               })
-            }}>
-            <Route exact path = {`${match.path}`} component = {ShopPageWithSearch}  />      
-            <Route path = {`${match.path}/:categoryId`} component = {CollectionPageContainer} /> 
-            </Profiler>    
-         </Suspense> 
-         </ErrorBoundary>       
-         </div>
-          )  }
-
-
-const mapDispatchToProps = dispatch => ({
-   fetchCollectionsStart : () => dispatch(fetchCollectionsStart())
-})
-
-
-
- export default connect(null, mapDispatchToProps) (ShopPage)
-
- //*  <Route exact path = {`${match.path}`} component = {CollectionsOverviewContainer}  />
- /*componentDidMount(){
-   const {fetchCollectionsStart} = this.props
-   fetchCollectionsStart()
-}
-   render() { 
-      
-      //if collection is null, seletor pass false, so we wanna pass true to withspiiner to start spinning, otherwise false if collecton is object
-   } 
-      
-      */
- 
- 
+export default connect(null, mapDispatchToProps)(ShopPage);
