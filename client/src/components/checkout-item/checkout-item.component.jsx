6adb56f51c81c55ac9728037bf9116d04780cd62
Simranjeet Singh <simranjeet.singh@thestable.com>
2022-09-14 14:43:29 -0400

    added amplify

--- client/src/components/checkout-item/checkout-item.component.jsx
+++ client/src/components/checkout-item/checkout-item.component.jsx
@@ -1,35 +1,38 @@
-import React from "react"
+import React from 'react';
 
-import "./checkout-item.styles.scss"
-import {connect} from "react-redux"
-import {clearCartItem, removeItem, addItem} from "../../redux/cart/cart.actions"
+import './checkout-item.styles.scss';
+import { connect } from 'react-redux';
+import { clearCartItem, removeItem, addItem } from '../../redux/cart/cart.actions';
 
-const CheckoutItem = ({cartItem, clearItemFromCart, addItem, removeItem}) => {
-    const {name, imageUrl, price, quantity} = cartItem  //if we destructure cartItem above, we dont have clearItem properties
-    return (
-    <div className = "checkout-item">
-        <div className = "image-container">
-            <img src= {imageUrl} alt="item"/>
-            </div>
-            <span className = "name">{name}</span>
-            <span className = "quantity">
-               <div className = "arrow" onClick = { () => removeItem(cartItem) }>&#10094;</div> 
-                  <div className = "value">{quantity}</div>
-                <div className = "arrow" onClick = { () => addItem(cartItem)}>&#10095;</div> 
-                </span>
-            <span className = "price">${price}</span>
-            <div className = "remove-button" onClick = {
-                () => clearItemFromCart(cartItem)}>&#10005;</div>
-           
-        
+const CheckoutItem = ({ cartItem, clearItemFromCart, addItem, removeItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem; //if we destructure cartItem above, we dont have clearItem properties
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>
+          &#10094;
+        </div>
+        <div className="value">{quantity}</div>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
+      </span>
+      <span className="price">${price}</span>
+      <div className="remove-button" onClick={() => clearItemFromCart(cartItem)}>
+        &#10005;
+      </div>
     </div>
+  );
+};
 
-)}
+const mapDispatchToProps = (dispatch) => ({
+  clearItemFromCart: (item) => dispatch(clearCartItem(item)),
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(removeItem(item))
+});
 
-const mapDispatchToProps = dispatch => ({
-    clearItemFromCart: item => dispatch(clearCartItem(item)),
-    addItem: item => dispatch(addItem(item)),
-    removeItem: item => dispatch(removeItem(item))
-})
-
-export default connect(null, mapDispatchToProps)(CheckoutItem)
+export default connect(null, mapDispatchToProps)(CheckoutItem);
